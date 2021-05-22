f90463324657accc0e3b6c7875ab4755345da6de
Apoorv Pranjal <appu2221@gmail.com>
2021-05-22 14:27:53 +0530

    Deleted all unnecessary files and added database to two tabs and frontend to all tabs

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+// ADDING DIFFERENT MODULES 
 const express = require("express");
 const cons = require('consolidate');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
+const { SIGHUP } = require("constants");
+mongoose.connect('mongodb://localhost/Devjam',{useNewUrlParser: true, useUnifiedTopology: true});
 const port = 80;
+
+//BUILDING SCHEMAS AND MODELS
+const signupSchema = new mongoose.Schema({
+    name: String,
+    hospital: String,
+    email: String,
+    contact: String,
+    materials: String
+  });
+const Signup = mongoose.model('Signup', signupSchema);
+const InNeed = mongoose.model('InNeed', signupSchema);
+const homeSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    contact: String,
+    address: String
+});
+const HomeService = mongoose.model('HomeService', homeSchema);
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static'))
@@ -14,7 +36,7 @@ app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-//ENDPOINTS
+//ENDPOINTS FOR NAVIGATION BAR
 app.get("/" ,(req,res) => {
     const params = { };
     res.status(200).render('index.html', params);
@@ -31,6 +53,24 @@ app.get("/homeService" ,(req,res) => {
     const params = { };
     res.status(200).render('homeService.html', params);
 })
+
+//ENDPOINTS FOR THE FORMS
+app.post('/signup', (req,res) => {  //for the signup page
+    let myData = new Signup(req.body);
+    myData.save().then(()=>{
+        res.status(200).render('data.html');
+    }).catch(()=>{
+        res.send("Couldn't reach the database")
+    });
+});
+app.post('/homeService', (req,res) => {    // for the homeservice page
+    let myData = new HomeService(req.body);
+    myData.save().then(()=> {
+        res.status(200).render('data.html');
+    }).catch(()=>{
+        res.send("Couldn't reach the database")
+    });
+});
 
 //STARTING THE SERVER
 app.listen(port,'127.0.0.1',()=> {
